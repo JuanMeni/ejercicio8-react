7fc2fa5e92ff836bfe258b49b474a334a421d29b
Juan Menichetti <jpmenichetti13@gmail.com>
2022-09-04 17:31:29 -0300

    preparo para subir a github

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,23 +1,26 @@
-import React from "react";
+import React,{useState} from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 
 const Formulario = () => {
+
+    
+
   return (
     <Form className="container" style={{backgroundColor:"rgb(207,244,252)"}}>
-      <Form.Group className="mb-3" controlId="formBasicNombre">
+      <Form.Group className="mb-3" controlId="nombre">
         <Form.Label>NOMBRE</Form.Label>
         <Form.Control type="text" placeholder="Pablo" />
       </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicApellido">
+      <Form.Group className="mb-3" controlId="apellido">
         <Form.Label>APELLIDO</Form.Label>
         <Form.Control type="text" placeholder="Perez" />
       </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicDni">
+      <Form.Group className="mb-3" controlId="dni">
         <Form.Label>DNI</Form.Label>
         <Form.Control type="number" placeholder="12345678" />
       </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
+      <Form.Group className="mb-3" controlId="email">
         <Form.Label>EMAIL</Form.Label>
         <Form.Control type="email" placeholder="pabloperez@gmail.com" />
       </Form.Group>
